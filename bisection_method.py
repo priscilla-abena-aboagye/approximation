3998e4d963d3f61bcc172885d991005c1d6a3938
Abena <priscillaabenaaboagye@gmail.com>
2025-01-29 11:27:25 +0000

    Creating an if statement

--- bisection_method.py
+++ bisection_method.py
@@ -1,3 +1,7 @@
 def square_root_bisection(square_target, tolerance = 1e-7, max_iteration = 100):
     if square_target < 0:
-        pass
+        raise ValueError("Square root of negative number is not defined in real numbers")
+    
+    if square_target == 1:
+        root = 1
+        print(f"The square root of {square_target} is 1")
