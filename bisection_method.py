f2f917dbdf2e7a5f3aa96956d4a114b8bbe92d04
Abena <priscillaabenaaboagye@gmail.com>
2025-01-29 12:14:29 +0000

    If statement to check the root

--- bisection_method.py
+++ bisection_method.py
@@ -1,4 +1,4 @@
-def square_root_bisection(square_target, tolerance = 1e-7, max_iteration = 100):
+def square_root_bisection(square_target, tolerance = 1e-7, max_iterations = 100):
     if square_target < 0:
         raise ValueError("Square root of negative number is not defined in real numbers")
     
@@ -12,6 +12,22 @@ def square_root_bisection(square_target, tolerance = 1e-7, max_iteration = 100):
         low = 0
         high = max(1, square_target)
         root = None
-        for _ in range(max_iteration):
+        for _ in range(max_iterations):
             mid = (low + high) / 2
             square_mid = mid ** 2
+            if abs(square_mid - square_target) < tolerance:
+                root = mid
+                break
+            elif square_mid < square_target:
+                low = mid
+            else:
+                high = mid
+        if root is None:
+            print(f"Failed to converge within {max_iterations} iterations.")
+        else:
+            print(f"The square root of {square_target} is approximately {root}")
+
+        return root
+
+N = 16
+square_root_bisection(N)
